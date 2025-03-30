1d374811d63a30552250789cffa66cfd93b4a254
prabhat <shankarprabhat121@gmail.com>
2025-03-30 19:59:53 +0530

    updated project files/structure of the project

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from "./components/Header"
+import { Tabs } from "./components/Tabs"
+import { TodoInput } from "./components/TodoInput"
+import { TodoList } from "./components/TodoList"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Tabs/>
+      <TodoList/>
+      <TodoInput/>
+    </>
+    
+
+  )
+}
+
+export default App
